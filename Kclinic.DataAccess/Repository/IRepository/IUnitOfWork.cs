9b3b695e85ce0779ecae0a0653994dbb857cf481
hoang la nhu <lanhuhoang261198@gmail.com>
2023-08-15 14:12:08 +0700

    edit

--- Kclinic.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Kclinic.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -11,6 +11,7 @@ namespace Kclinic.DataAccess.Repository.IRepository
         ICategoryRepository Category {  get; }
         ICoverTypeRepository CoverType {  get; }
         IBlogRepository Blog { get; }
+        IProductRepository Product { get; }
 
         void Save();
     }
--- Kclinic.DataAccess/Repository/UnitOfWork.cs
+++ Kclinic.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using Kclinic.DataAccess.Repository.IRepository;
+using Kclinic.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +18,12 @@ namespace Kclinic.DataAccess.Repository
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
             Blog = new BlogRepository(_db);
+            Product = new ProductRepository(_db);
         }
         public ICategoryRepository Category { get; private set; }
         public ICoverTypeRepository CoverType {  get; private set; }
         public IBlogRepository Blog { get; private set; }
+        public IProductRepository Product { get; private set; }
         public void Save()
         {
             _db.SaveChanges();
